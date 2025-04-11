7ef7cf3ea8c0d596c5d42b873a2dca1f384f7db9
hajaemin <woals3527@naver.com>
2025-04-11 19:25:05 +0900

    스프링 세팅 완료

---
+++ src/main/java/org/hajam/controller/HelloController.java
@@ -0,0 +1,22 @@
+package org.hajam.controller;
+
+import org.hajam.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println("hello...." + helloService);
+    }
+}
---
+++ src/main/java/org/hajam/service/HelloService.java
@@ -0,0 +1,8 @@
+package org.hajam.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+}
